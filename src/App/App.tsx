252421fd4cc06f81b26ae63c2a39b3727da6f434
Anoop Hallur <anoophallur@gmail.com>
2021-06-17 00:34:06 +0530

    Changing to typescript + react

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import "./App.css";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className={"body"}>
+        Hello World
+      </div>
+    );
+  }
+}
